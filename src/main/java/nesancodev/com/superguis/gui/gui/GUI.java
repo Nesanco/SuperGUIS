9073d57a4353949f0773f4d39582f0cf182acad2
Nesanco <80917510+Nesanco@users.noreply.github.com>
2022-08-21 16:54:32 -0400

    Add files via upload

--- src/main/java/nesancodev/com/superguis/gui/gui/GUI.java
+++ src/main/java/nesancodev/com/superguis/gui/gui/GUI.java
@@ -1,21 +1,27 @@
 package nesancodev.com.superguis.gui.gui;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class GUI {
     private Inventory inv;
     private String format;
     private GUIType type;
+    private String title;
+    private HashMap<String, Animation> animations = new HashMap<>();
     private HashMap<String, ItemStack> formatMap = new HashMap<>();
 
     public GUI(int size, String title, GUIType type) {
         this.inv = Bukkit.createInventory(null, size, title);
         this.type = type;
+        this.title = title;
     }
 
     public Inventory getInventoryRaw() {
@@ -30,8 +36,54 @@ public class GUI {
         return type;
     }
 
+    public ItemStack getItem(Integer slot) {
+        return inv.getItem(slot);
+    }
+
+    public ArrayList<ItemStack> getAll(ItemStack stack) {
+        ArrayList<ItemStack> stacks = new ArrayList<>();
+        for (ItemStack x : inv.getContents()) {
+            if (x == stack) {
+                stacks.add(x);
+            }
+        }
+        return stacks;
+    }
+
+    public void remove(Integer i) {
+        inv.clear(i);
+    }
+
+    public HashMap<Integer, ItemStack> getAll() {
+        HashMap<Integer, ItemStack> stackHashMap = new HashMap<>();
+        for (int i = 0; i < inv.getSize(); i++) {
+            stackHashMap.put(i, inv.getItem(i));
+        }
+        return stackHashMap;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<HumanEntity> getViewers() {
+        return inv.getViewers();
+    }
+
+    public Integer getSize() {
+        return inv.getSize();
+    }
+
+    public ItemStack[] getItems() {
+        return inv.getContents();
+    }
+
     public void addFormat(String text, ItemStack stack) {
         formatMap.put(text, stack);
+    }
+
+    public void formatSlot(Integer slot, ItemStack stack) {
+        inv.setItem(slot, stack);
     }
 
     public void open(Player p) {
