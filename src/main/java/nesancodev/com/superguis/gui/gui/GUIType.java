055cbdd5c75265f6b9a0623159c76814cde94e66
Nesanco <80917510+Nesanco@users.noreply.github.com>
2022-08-26 15:41:28 -0400

    Add files via upload

---
+++ src/main/java/nesancodev/com/superguis/gui/gui/GUIType.java
@@ -0,0 +1,19 @@
+package nesancodev.com.superguis.gui.gui;
+
+public enum GUIType {
+    CHEST,
+    HOPPER,
+    SHULKER,
+    SMOKER,
+    BEACON,
+    BREWING_STAND,
+    ENCHANTING_TABLE,
+    ANVIL,
+    ENDER_CHEST,
+    FURNACE,
+    BARREL,
+    CRAFTING_TABLE,
+    LOOM,
+    CARTOGRAPHY_TABLE,
+    DEFAULT
+}
